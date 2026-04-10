5f6bb10e6ad026d58a84fff986cc7e9d5e738de8
ANJAN SAI MERLA <merlasrianjanasai@gmail.com>
2026-04-10 18:48:00 +0530

    chore: update CORS origin to https://clientflow-eta.vercel.app/

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://clientflow-pi.vercel.app"],
+        origin: ["http://localhost:5173", "https://clientflow-eta.vercel.app/"],
         methods: ["GET", "POST"]
     }
 });
